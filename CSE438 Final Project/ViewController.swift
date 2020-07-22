58d7c769e2263ba3b4b284ffbd07a04557113d95
wert1128 <michael.zhao@wustl.eddu>
2020-07-22 14:35:24 -0400

    Initial Commit

---
+++ CSE438 Final Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSE438 Final Project
+//
+//  Created by Michael Zhao on 7/22/20.
+//  Copyright © 2020 Michael Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CSE438 Final ProjectTests/CSE438_Final_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  CSE438_Final_ProjectTests.swift
+//  CSE438 Final ProjectTests
+//
+//  Created by Michael Zhao on 7/22/20.
+//  Copyright © 2020 Michael Zhao. All rights reserved.
+//
+
+import XCTest
+@testable import CSE438_Final_Project
+
+class CSE438_Final_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
