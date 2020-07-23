b571fe0eb02787106394b87cf7ade1ba33bc7f39
MLHLocalhost <localhost@mlh.io>
2020-07-23 13:13:44 -0500

    course_detail

---
+++ CSE438 Final Project/Course.swift
@@ -0,0 +1,9 @@
+//
+//  Course.swift
+//  CSE438 Final Project
+//
+//  Created by 王梓辰 on 7/23/20.
+//  Copyright © 2020 Michael Zhao. All rights reserved.
+//
+
+import Foundation
---
+++ CSE438 Final Project/CourseDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CourseDetailViewController.swift
+//  CSE438 Final Project
+//
+//  Created by 王梓辰 on 7/23/20.
+//  Copyright © 2020 Michael Zhao. All rights reserved.
+//
+
+import UIKit
+
+class CourseDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
