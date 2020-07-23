7fea88aaa70d3f73d89f5722664fb9019ba81b74
wert1128 <michael.zhao@wustl.eddu>
2020-07-22 22:46:14 -0400

    search view controller prelim

--- CSE438 Final Project/SearchViewController.swift
+++ CSE438 Final Project/SearchViewController.swift
@@ -9,12 +9,35 @@
 import Foundation
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    @IBOutlet weak var departmentCollectionView: UICollectionView!
+    var departments:[department]=[]
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return departments.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! departmentCell
+        return cell
+    }
+    
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        departmentCollectionView.delegate=self
+        departmentCollectionView.delegate=self
+        departmentCollectionView.register(departmentCell.self, forCellWithReuseIdentifier: "Cell")
+        departmentCollectionView.isUserInteractionEnabled = true
+        getDepartments(school:"Arts&Sci")
         // Do any additional setup after loading the view.
+        departmentCollectionView.reloadData()
+    }
+    
+    func getDepartments(school:String){
+        //yet to be implemented
+        //get from database the lists of departments given a specific school, and populate them into the departments array
     }
     
     
---
+++ CSE438 Final Project/departmentCell.swift
@@ -0,0 +1,14 @@
+//
+//  departmentCell.swift
+//  CSE438 Final Project
+//
+//  Created by Michael Zhao on 7/22/20.
+//  Copyright © 2020 Michael Zhao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class departmentCell:UICollectionViewCell{
+    var department:department!
+}
